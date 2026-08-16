c0d67a5d7a3ce12b32f86376255cf00fb79b1c8e
harshit29062006 <harshit29062006@gmail.com>
2026-08-16 19:46:33 +0530

    Add files via upload

---
+++ Lab_1/Power.java
@@ -0,0 +1,24 @@
+// Write Java Program to Calculate the Power of a Number 
+
+import java.util.Scanner;
+
+public class Power{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the base: ");
+        int base = sc.nextInt();
+        System.out.print("Enter the exponent: ");
+        int exponent = sc.nextInt();
+
+        int result = 1;
+
+        for (int i = 1; i <= exponent; i++){
+            result = result * base;
+        }
+
+        System.out.println(base + " raised to the power " + exponent + " = " + result);
+        sc.close();
+    }
+}
+
